41d40ed7533a56a2e7c975f2e3795585a67d1b47
Hervé TEGUIA <tkh.tegus@gmail.com>
2023-03-02 14:56:17 +0400

    up

--- MpaGoPay.Web/Data/WeatherForecast.cs
+++ MpaGoPay.Web/Data/WeatherForecast.cs
@@ -6,6 +6,8 @@ namespace MpaGoPay.Web.Data
 
         public int TemperatureC { get; set; }
 
+
+
         public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
 
         public string? Summary { get; set; }
