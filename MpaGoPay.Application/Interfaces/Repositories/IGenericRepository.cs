81325c1671d0b7c20baecf41e3063769dbd67767
Hervé TEGUIA <tkh.tegus@gmail.com>
2023-03-04 09:56:05 +0400

    up

---
+++ MpaGoPay.Application/Interfaces/Repositories/IGenericRepository.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MpaGoPay.Application.Interfaces.Specifications;
+using MpaGoPay.Domain.Entities;
+
+namespace MpaGoPay.Application.Interfaces.Repositories
+{
+    public interface IGenericRepository<T> where T : BaseEntity
+    {
+        Task<IReadOnlyList<T>> GetAllAsync();
+        Task<T> GetByIdAsync(long id);
+
+        Task<T> GetEntityWithSpec(ISpecifications<T> specification);
+        Task<IReadOnlyList<T>> ListAsync(ISpecifications<T> specification);
+        Task<int> CountAsync(ISpecifications<T> specifications);
+        Task DeleteAsync(T entity);
+        Task UpdateAsync(T entity);
+
+        Task Add(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+    }
+}
---
+++ MpaGoPay.Domain/Enums/CustomType.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MpaGoPay.Domain.Enums
+{
+    public enum MeansOfPayment : byte
+    {
+        MTN_MOBILE_MONEY = 1,
+        ORANGE_MONEY = 2
+    }
+}
